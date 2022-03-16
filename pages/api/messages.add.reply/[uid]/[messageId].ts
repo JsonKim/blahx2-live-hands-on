430246965bcf06dbe99a8883b3b22e1f557c4f10
Jason Kim <actcrazy@gmail.com>
2022-03-16 21:39:45 +0900

    feat: 메시지 등록, 목록, 댓글 api 추가

--- pages/api/messages.add.reply/[uid]/[messageId].ts
+++ pages/api/messages.add.reply/[uid]/[messageId].ts
@@ -1,10 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import MessageCtrl from '@/controllers/message/message.controller';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
   console.info(method);
-  // TODO: 여기부터 시작
-  res.status(400).end();
+  try {
+    await MessageCtrl.postReplay(req, res);
+    res.status(200).end();
+  } catch (error) {
+    console.error(error);
+    res.status(400).end();
+  }
 }
